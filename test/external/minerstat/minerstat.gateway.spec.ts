204fe1e40b00743587bc96fd79ae97bba61e5b2b
Gustavo H. Lopes de Souza <gustavo.hlopess@gmail.com>
2022-03-04 21:11:55 -0300

    refactor: change test to deal with pools instead of coins

--- test/external/minerstat/minerstat.gateway.spec.ts
+++ test/external/minerstat/minerstat.gateway.spec.ts
@@ -9,20 +9,27 @@ const mockedAxios = jest.mocked(axios);
 describe('MinerstatGateway', () => {
     const gateway = new MinerstatGateway();
 
-    describe('#listCoins()', () => {
+    describe('#listRewardableMiningPools()', () => {
         describe('when call to Minerstat API is successful', () => {
-            let expectedListOfCoins = [ Builder<CoinDto>().build() ];
-            
-            beforeAll(() => {
-                mockedAxios.get = jest.fn().mockResolvedValue({
-                    data: expectedListOfCoins
-                });
-            });
+            describe('and the response contains one or more pool data', () => {
+                const apiResponse = [ 
+                    Builder<CoinDto>().type('pool').build(),
+                    Builder<CoinDto>().type('coin').build()
+                ];
 
-            it('should return a list of coins', async () => {
-                const listOfCoins = await gateway.listCoins();
-                expect(mockedAxios.get).toHaveBeenCalledWith(`${process.env.MINERSTAT_URL}/v2/coins`);
-                expect(listOfCoins).toEqual(expectedListOfCoins);
+                const expectedListOfPools = [apiResponse[0]];
+            
+                beforeAll(() => {
+                    mockedAxios.get = jest.fn().mockResolvedValue({
+                        data: apiResponse
+                    });
+                });
+    
+                it('should return a list containing data of mining pools', async () => {
+                    const listOfCoins = await gateway.listRewardableMiningPools();
+                    expect(mockedAxios.get).toHaveBeenCalledWith(`${process.env.MINERSTAT_URL}/v2/coins`);
+                    expect(listOfCoins).toEqual(expectedListOfPools);
+                });
             });
         });
 
@@ -32,7 +39,7 @@ describe('MinerstatGateway', () => {
             });
 
             it('should throw an error', async () => {
-                await expect(gateway.listCoins()).rejects.toThrow('Error');
+                await expect(gateway.listRewardableMiningPools()).rejects.toThrow('Error');
             });
         })
     });
